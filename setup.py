ff1c432338c341fa24c3d6e445833d7b6ba2a69a
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-07-03 21:19:49 -0600

    more classifiers and package data

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
 from setuptools import setup, find_packages
 
-REPO_BASE_URL = "https://github.com/Sebastian-Soto-M/pyman"
-with open("README.md", "r", encoding="utf-8") as fh:
+REPO_BASE_URL = 'https://github.com/Sebastian-Soto-M/pyman'
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
-    name="pyman",
-    version="0.0.1",
-    author="Sebastian Soto Madrigal",
-    author_email="s.m.sebastian.n@gmail.com",
-    description="Python helper for package & project creation",
+    name='pyman',
+    version='0.0.1',
+    author='Sebastian Soto Madrigal',
+    author_email='s.m.sebastian.n@gmail.com',
+    description='Python helper for package & project creation',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url=REPO_BASE_URL,
     project_urls={
-        "Bug Tracker": f"{REPO_BASE_URL}/issues",
-        "Pull Requests": f"{REPO_BASE_URL}/pulls",
+        'Bug Tracker': f'{REPO_BASE_URL}/issues',
+        'Pull Requests': f'{REPO_BASE_URL}/pulls',
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators'
+        'License :: OSI Approved :: MIT License',
     ],
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    python_requires=">=3.8",
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    python_requires='>=3.9',
 )
